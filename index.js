fc83f6bdcadb5dd5a346d563698dec56b3b9db7d
Sanskar Jain <sanskar94511@gmail.com>
2020-10-01 16:43:56 +0530

    Login auth. and mail verification

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ var bodyParser = require('body-parser');
 const fs=require("fs")
 const path=require("path")
 const app = express();
+var mongoose = require('mongoose');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'))
 app.use("/static",express.static('static')) ;
 app.set("view engine", "ejs");
 const hostname = 'localhost';
 const port = 27017;
-
+mongoose.connect('mongodb://localhost:27017/StrangeFlix', {useNewUrlParser: true,useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('DB connected');
+});
 
 
 //Router
--- modules/login.js
+++ modules/login.js
@@ -1,8 +1,43 @@
-const router = require('express').Router();
-router.get('/login', (req, res)=>{
-    res.status(200).render('login.ejs',{
-      "error": "",
-      "message": ""
-    });
+const routerLogin = require('express').Router();
+var mongoose = require('mongoose');
+var userdata = mongoose.model('registration');
+routerLogin.get('/login', (req, res) => {
+  res.status(200).render('login.ejs', {
+    "error": "",
+    "message": ""
+  });
+});
+
+routerLogin.post('/login',(req,res)=>{
+    var Post=req.body.post;
+    var Email=req.body.email;
+    var Password=req.body.password;
+    if (Email == "" || Password == "") {
+      res.status(200).render('login.ejs', {
+        "error": "Fill all the fields below",
+        "message": ""
+      });
+      return;
+    }
+    userdata.findOne({email:Email,password:Password},(err,data)=>{
+      if (err)
+      console.log(err);
+      else{
+        console.log(data);
+        if(data!=null)
+        {
+          res.status(200).render('user.ejs');
+        }
+        else{
+          res.status(200).render('login.ejs', {
+            "error": "Invalid Details",
+            "message": ""
+          });
+        }
+      }
+    })
 })
-module.exports = router;
+
+
+
+module.exports = routerLogin;
