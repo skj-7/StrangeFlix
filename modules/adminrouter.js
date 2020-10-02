8578fd0e4eb4b11aa40a3a08ef16dc34a9a96b4d
Sanskar Jain <sanskar94511@gmail.com>
2020-10-02 21:13:30 +0530

    admin-main page

---
+++ modules/adminrouter.js
@@ -0,0 +1,27 @@
+const adminrouter = require('express').Router();
+
+const pin = 2020;
+adminrouter.get('/admin', (req, res) => {
+    res.status(200).render('Adminmain.ejs', {
+        "error": "",
+        "message": ""
+    });
+})
+adminrouter.post('/admin', (req, res) => {
+    const Pin = req.body.pin;
+    if (pin != Pin) {
+        res.status(200).render('Adminmain.ejs', {
+            "error": "Invalid Pin",
+            "message": ""
+        });
+    }
+    else {
+        res.status(200).render('Adminpage.ejs');
+    }
+})
+
+
+
+
+
+module.exports = adminrouter;
--- modules/router.js
+++ modules/router.js
@@ -3,11 +3,15 @@ const router = require('express').Router();
 regRouter = require("./register");
 loginRouter = require("./login");
 verifRouter = require("./verification");
+adminRouter = require("./adminrouter");
 
 router.use("/register", regRouter);
 
 router.use("/login", loginRouter);
 
 router.use("/confirmation", verifRouter);
+
+router.get("/admin",adminRouter);
+router.post("/admin",adminRouter);
 
 module.exports = router;
