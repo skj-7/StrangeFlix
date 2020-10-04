c90917c828910de521872319fb029d0fa600ad80
Priyanshu Gupta <priyanshusmvs@gmail.com>
2020-10-04 18:45:43 +0530

    Added Schemas for Video and Series

---
+++ schemas/videos.js
@@ -0,0 +1,67 @@
+var mongoose = require('mongoose');
+
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+const durationSchema = new mongoose.Schema({
+	hours: {
+		type: Number,
+		min: 0,
+		max: 10,
+		default: 0
+	},
+	minutes: {
+		type: Number,
+		min: 0,
+		max: 59,
+		required: true
+	},
+	seconds: {
+		type: Number,
+		min: 0,
+		max: 59,
+		required: true
+	}
+});
+
+const videoSchema = new mongoose.Schema({
+	_seriesId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Series',
+		default: null
+	},
+	title: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	price: {
+		type: Currency,
+		required: true,
+		min: 0,
+		max: 5000000
+	},
+	thumbnail: {
+		type: String,
+		required: true
+	},
+	category: {
+		type: String,
+		required: true
+	},
+	filepath: {
+		type: String,
+		required: true
+	},
+	duration: durationSchema,
+	tags: [String]
+}, {
+	timestamps: true
+});
+
+var video = mongoose.model('Videos', videoSchema);
+
+module.exports = video;
