407967dc6c361cea5defde9365c6e51499ca8c43
Priyanshu Gupta <priyanshusmvs@gmail.com>
2020-10-04 14:58:56 +0530

    Some renames

--- modules/adminrouter.js
+++ modules/adminrouter.js
@@ -1,27 +1,23 @@
 const adminrouter = require('express').Router();
 
 const pin = 2020;
-adminrouter.get('/admin', (req, res) => {
-    res.status(200).render('Adminmain.ejs', {
+adminrouter.get('/', (req, res) => {
+    res.status(200).render('adminlogin.ejs', {
         "error": "",
         "message": ""
     });
 })
-adminrouter.post('/admin', (req, res) => {
+adminrouter.post('/', (req, res) => {
     const Pin = req.body.pin;
     if (pin != Pin) {
-        res.status(200).render('Adminmain.ejs', {
+        res.status(200).render('adminlogin.ejs', {
             "error": "Invalid Pin",
             "message": ""
         });
     }
     else {
-        res.status(200).render('Adminpage.ejs',{"videoarray":"","series":""});
+        res.status(200).render('admincontrol.ejs',{"videoarray":"","series":""});
     }
 })
-
-
-
-
 
 module.exports = adminrouter;
