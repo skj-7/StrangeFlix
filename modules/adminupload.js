60fe523632d99403729a10457db540a00c4b5a98
wsLord <priyanshusmvs@gmail.com>
2020-10-21 18:08:34 +0530

    Added working backend for normal file upload

--- modules/adminupload.js
+++ modules/adminupload.js
@@ -2,7 +2,7 @@ const adminupload = require('express').Router();
 
 const fs = require("fs");
 const fse = require('fs-extra');
-// const youtubedl = require('youtube-dl')
+const youtubedl = require('youtube-dl')
 const multer = require('multer');
 const path = require('path');
 const videos = require('../schemas/videos');
@@ -21,7 +21,7 @@ var storage = multer.diskStorage({
 		}
 	},
 	filename: (req, file, cb) => {
-		cb(null, file.fieldname + path.extname(file.originalname));
+		cb(null, 'undefined' + path.extname(file.originalname));
 	}
 });
 
@@ -41,119 +41,124 @@ adminupload.get('/', (req, res) => {
 	}
 });
 
-adminupload.post('/checkseries', (req, res) => {
-	var Name = req.body.SeriesName;
+// adminupload.post('/checkseries', (req, res) => {
+// 	var Name = req.body.SeriesName;
 
-	videoSeries.findOne({ seriesTitle: Name }, (error, data) => {
-		if (error)
-			return console.log(error);
-		else if (data == null) {
-			res.json({ "isPresent": "false" });
-		}
-		else
-			res.json({ "isPresent": "true" });
-	});
-});
+// 	videoSeries.findOne({ seriesTitle: Name }, (error, data) => {
+// 		if (error)
+// 			return console.log(error);
+// 		else if (data == null) {
+// 			res.json({ "isPresent": "false" });
+// 		}
+// 		else
+// 			res.json({ "isPresent": "true" });
+// 	});
+// });
+
+var saveData = (body, series_id) => {
+
+}
 
 adminupload.post('/', (req, res) => {
-	upload(req, res, function (err) {
-		if (err) {
-			console.log(err);
-			res.render('error.ejs', { "message": "", "error": "Unexpected error Occured!" });
-		}
-		else {
-			var Title = req.body.title;
-			var Price = req.body.price;
-			var Dhour = req.body.hours;
-			var Dmin = req.body.minutes;
-			var Dsec = req.body.seconds;
-			var SeriesName = req.body.series;
-			var Descr = req.body.description;
-			var Tags = req.body.tags.split(',');
-			var Category = req.body.category;
+	if (req.session.admin) {
+		upload(req, res, function (err) {
+			if (err) {
+				console.log(err);
+				res.render('error.ejs', { "message": "", "error": "Unexpected error Occured!" });
+			}
+			else {
+				var Title = req.body.title;
+				var Price = req.body.price;
+				var Dhour = req.body.hours;
+				var Dmin = req.body.minutes;
+				var Dsec = req.body.seconds;
+				var SeriesName = req.body.series;
+				var Descr = req.body.description;
+				var Tags = req.body.tags.split(',');
+				var Category = req.body.category;
 
-			videoSeries.findOne({ seriesTitle: SeriesName }, (error, data) => {
-				if (error) {
-					console.log(error);
-					res.render('error.ejs', { "message": "", "error": "Unexpected error Occured!" });
-				}
-				else if (data == null) {
-					res.render('adminUpload.ejs', {
-						"error": "",
-						"message": ""
-					});
-				}
-				else {
-					if (typeof req.files.video == 'undefined') {
-						console.log("No video selected!")
+				videoSeries.findOne({ seriesTitle: SeriesName }, (error, data) => {
+					if (error) {
+						console.log(error);
+						res.render('error.ejs', { "message": "", "error": "Unexpected error Occured!" });
 					}
-					// else {
+					else if (data == null && SeriesName != "") {
+						res.render('adminUpload.ejs', {
+							"error": "Series Name: " + name + " does not exist. Register <a href=" + req.headers.host + "\/admin\/regseries> here </a>.",
+							"message": ""
+						});
+					}
+					else {
+						var savedata = new videos({
+							_seriesId: data ? data._id : null,
+							title: Title,
+							description: Descr,
+							price: Price,
+							thumbnail: "/test",
+							category: Category,
+							filepath: "/test",
+							duration: {hours: Dhour, minutes: Dmin, seconds: Dsec},
+							tags: Tags,
+						});
+						savedata.save( (err) => {
+							if (err)
+								return console.error(err);
+							else {
+								var video_id = savedata._id;
+								if (typeof req.files.video == 'undefined') {
+									console.log("No video selected!")
+									var YTLink = req.body.ytlink;
+									var AS3Link = req.body.AS3link;
+									if(YTLink != "") {
+										const videodown = youtubedl(YTLink, ['--format=18'], { cwd: __dirname });
+										videodown.on('info', function (info) {
+											console.log('Download started')
+											console.log('filename: ' + info._filename)
+											console.log('size: ' + info.size)
+											// videodown.pipe(fs.createWriteStream(info._filename + '.mp4'));
+										})
 
-
-					// }
-				}
-			});
-		}
-	});
+										// videodown.on('end', async () => {
+										// 	await console.log('finished downloading!');
+										// 	await res.render('adminUpload.ejs', { "message": "YT Video: " + Title + " successfully uploaded!", "error": "" });
+										// })
+									}
+									else if(AS3Link != ""){
+										res.send("Ruk abhi!");
+									}
+								}
+								else {
+									fs.renameSync(req.files.video[0].path, req.files.video[0].path.replace('undefined', video_id));
+									fs.renameSync(req.files.thumbnail[0].path, req.files.thumbnail[0].path.replace('undefined', video_id));
+									videos.findByIdAndUpdate(video_id, {
+										"thumbnail": req.files.thumbnail[0].path.replace('undefined', video_id),
+										"filepath": req.files.video[0].path.replace('undefined', video_id)
+									}, (error, result) => {
+										if(error) {
+											console.log(error);
+											res.render('error.ejs', { "message": "Unexpected error Occured!", "error": error })
+										}
+										else {
+											res.render('adminUpload.ejs', { "message": "File: " + Title + " successfully uploaded!", "error": "" });
+										}
+									});
+								}
+							}
+						});
+					}
+				});
+			}
+		});
+	}
+	else {
+		res.redirect('/admin/login');
+	}
 });
 
 module.exports = adminupload;
 
-// 	adminupload.get('/download', (req, res) => {
-// 		var url = req.query.ytlink;
+	adminupload.get('/download', (req, res) => {
+		var url = req.query.ytlink;
 
-// 		const video = youtubedl(url, ['--format=18'], { cwd: __dirname });
-// 		video.on('info', function (info) {
-// 			console.log('Download started')
-// 			console.log('filename: ' + info._filename)
-// 			console.log('size: ' + info.size)
-// 			video.pipe(fs.createWriteStream(info._filename + '.mp4'));
-// 		})
-
-// 		video.on('end', async () => {
-// 			await console.log('finished downloading!');
-// 			await res.status(200).render('upload.ejs', { "message": "uploaded" });
-// 		})
-// 	})
-
-// 	const output = 'myvideo.mp4'
-
-// 	let downloaded = 0
-
-// 	if (fs.existsSync(output)) {
-// 		downloaded = fs.statSync(output).size
-// 	}
-
-// 	const video = youtubedl('https://www.youtube.com/watch?v=179MiZSibco',
-// 		['--format=18'],
-// 		{ start: downloaded, cwd: __dirname })
-
-// 	// Will be called when the download starts.
-// 	video.on('info', function (info) {
-// 		console.log('Download started')
-// 		console.log('filename: ' + info._filename)
-
-// 		// info.size will be the amount to download, add
-// 		let total = info.size + downloaded
-// 		console.log('size: ' + total)
-
-// 		if (downloaded > 0) {
-// 			// size will be the amount already downloaded
-// 			console.log('resuming from: ' + downloaded)
-
-// 			// display the remaining bytes to download
-// 			console.log('remaining bytes: ' + info.size)
-// 		}
-// 	})
-
-// 	video.pipe(fs.createWriteStream(output, { flags: 'a' }))
-
-// 	// Will be called if download was already completed and there is nothing more to download.
-// 	video.on('complete', function complete(info) {
-// 		'use strict'
-// 		console.log('filename: ' + info._filename + ' already downloaded.')
-// 	})
-
-// 	video.on('end', function () {
-// 		console.log('finished downloading!')
-// 	})
+		
+	})
