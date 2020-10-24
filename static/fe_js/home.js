44688d64fe02f43b966365aebeff0199199e8324
Sanskar Jain <sanskar94511@gmail.com>
2020-10-24 14:39:44 +0530

    added responsiveness

--- static/fe_js/home.js
+++ static/fe_js/home.js
@@ -1,10 +1,13 @@
 var modal = document.getElementById("myModal");
 
 var btn = document.getElementById("myBtn");
-
+var btn1 = document.getElementById("myBtn1");
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn1.onclick = function() {
   modal.style.display = "block";
 }
 
@@ -14,5 +17,18 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+function menu()
+{
+  var menu=document.getElementById('menubar');
+  if(menu.style.display=="none")
+  {
+    menu.style.display="flex";
+  }
+  else
+  {
+    menu.style.display="none";
   }
 }
