04bb373724417b77ddccdb064123f585bdd7848d
Sanskar Jain <sanskar94511@gmail.com>
2020-10-29 22:59:24 +0530

    Added video & series details-update feature

--- modules/admin/admineditvideo.js
+++ modules/admin/admineditvideo.js
@@ -8,16 +8,35 @@ admineditvideo.use(bodyParser.json());
 admineditvideo.get('/:vidID', (req, res) => {
     if (req.session.admin) {
         videos.findOne({ _id: req.params.vidID }).populate('comments')
-        .exec( (err, vid) => {
-            if(err) {
-				return console.log(err);
-            }
-            
-            res.render('adminVideoEdit.ejs', {"video": vid});
-        })
+            .exec((err, vid) => {
+                if (err) {
+                    return console.log(err);
+                }
+
+                res.render('adminVideoEdit.ejs', { "video": vid });
+            })
     }
     else
         res.redirect('/admin/login');
 });
+
+admineditvideo.post('/:vidID', (req, res) => {
+    if (req.session.admin) {
+        videos.findOneAndUpdate({ _id: req.params.vidID },req.body, (err, data) => {
+            if (err) {
+                return console.log(err);
+            }
+            videos.findOne({ _id: req.params.vidID }).populate('comments')
+                .exec((err, vid) => {
+                    if (err) {
+                        return console.log(err);
+                    }
+
+                    res.render('adminVideoEdit.ejs', { "video": vid });
+                })
+        });
+    }
+});
+
 
 module.exports = admineditvideo;
