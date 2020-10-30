9f6aac40c1d811b848c987e5ea06429e5d6335dd
Sanskar Jain <sanskar94511@gmail.com>
2020-10-31 02:05:24 +0530

    Flagged videos and comments added to admin panel

--- modules/admin/adminFlagRecord.js
+++ modules/admin/adminFlagRecord.js
@@ -1,6 +1,31 @@
 const flagRecord = require('express').Router();
-flagRecord .get('/', (req, res) => {
-    res.render('flags-record.ejs',{"videoarray":'',"comments":""});
+const videos = require('../../schemas/videos');
+const flags = require('../../schemas/flags');
+const comments = require('../../schemas/comment');
+
+flagRecord.get('/', (req, res) => {
+    if (req.session.admin) {
+        var Flagged = [];
+        var FlaggedVid = [];
+        flags.find({ "flagtype": 0 }, (err, com) => {
+            com.forEach(x => {
+                Flagged.push({ "_id": x.flagid });
+            });
+            comments.find({ $or: Flagged }, (er, COMMENT) => {
+
+                flags.find({ "flagtype": 1 }, (e, vid) => {
+                    vid.forEach(x => {
+                        FlaggedVid.push({ "_id": x.flagid });
+                    });
+                    videos.find({$or: FlaggedVid}, (Er, VIDEO) => {
+                        res.render('flags-record.ejs', { "videoarray": VIDEO, "comments": COMMENT });
+                    });
+                }); 
+            });
+        });
+    }
+    else
+        res.redirect('/admin/login');
 });
 
 
